2ac25ceaf7803e117d0fbe27d024c6982423f19b
Jaeseong Lee <49643709+Jasonlee1995@users.noreply.github.com>
2024-01-08 09:20:54 +0900

    [ADD] collections counter example

---
+++ collections_example.py
@@ -0,0 +1,31 @@
+'''
+DOCS : https://docs.python.org/3/library/collections.html
+
+'''
+
+import collections
+
+
+def make_counter(lst):
+    return collections.Counter(lst)
+
+def get_most_common(counter, num):
+    return counter.most_common(num)
+
+
+if __name__ == '__main__':
+    abc_lst = ['a', 'a', 'a', 'b', 'c', 'c']
+    ab_lst  = ['a', 'a', 'b']
+
+    abc_cnt = make_counter(abc_lst)
+    ab_cnt  = make_counter(ab_lst)
+
+    print(abc_cnt, get_most_common(abc_cnt, 1))
+    print(ab_cnt, get_most_common(ab_cnt, 1))
+    print(abc_cnt - ab_cnt)
+
+    '''print result
+    Counter({'a': 3, 'c': 2, 'b': 1}) [('a', 3)]
+    Counter({'a': 2, 'b': 1}) [('a', 2)]
+    Counter({'c': 2, 'a': 1})
+    '''
